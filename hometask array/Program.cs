b20d647e8e904cabd927afd5b0a4e8a0b8613a89
mamedovx999 <mamedovx999@code.edu.az>
2024-02-17 02:09:50 +0400

    Proje dosyası ekle.

---
+++ hometask array/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Hello, World!");
+#region  Task1
+//int[] numbers={15,25,34,56,78};
+//int sum = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{  
+//        sum += numbers[i];
+//}
+//Console.WriteLine(sum);
+#endregion
+#region Task2
+//int[] numbers = { 45, 56, 35, 67, 32, 56 };
+//int max = numbers [0];
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] > max)
+//    {
+//        max = numbers[i];
+//    }
+//}    
+//    Console.WriteLine(max);
+#endregion
+#region Task3
+//int[] numbers = { 14, 24, 56, 43, 24 };
+//int sum = numbers[0]+ numbers [numbers.Length-1];
+//Console.WriteLine(sum);
+#endregion
+
